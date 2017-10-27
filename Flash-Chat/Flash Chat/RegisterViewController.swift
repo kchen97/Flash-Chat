6522ec4bcb271395e574ed46928d917e480008c2
Korman Chen <kormanchen@Kormans-MacBook-Pro.local>
2017-10-27 11:46:58 -0700

    Users can now be registered to firebase

---
+++ Flash-Chat/Flash Chat/RegisterViewController.swift
@@ -0,0 +1,52 @@
+//
+//  RegisterViewController.swift
+//  Flash Chat
+//
+//  This is the View Controller which registers new users with Firebase
+//
+
+import UIKit
+import Firebase
+
+class RegisterViewController: UIViewController, UITextFieldDelegate{
+
+    
+    //Pre-linked IBOutlets
+
+    @IBOutlet var emailTextfield: UITextField!
+    @IBOutlet var passwordTextfield: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        emailTextfield.delegate = self
+        passwordTextfield.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //Hides keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+  
+    @IBAction func registerPressed(_ sender: AnyObject) {
+        //TODO: Set up a new user on our Firbase database
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            FIRAuth.auth()?.createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!, completion: { (user, error) in
+                if error != nil {
+                    print(error)
+                }
+                else {
+                    print("Registration successful")
+                    self.performSegue(withIdentifier: "goToChat", sender: self)
+                }
+            })
+        }
+    } 
+    
+    
+}
